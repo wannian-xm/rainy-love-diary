20eb8b111b1ce101c7e89c73152499a775f7f098
wannian-xm <1356484263@qq.com>
2025-06-14 05:23:56 +0800

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div style={{ color: '#d0e0ff', background: '#1f2937', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '20px' }}>
-      <p>“能永远纠缠在一起吗？字节、命运、我和你。”<br/>—— 小满和惊蛰的雨天日记 🌧️</p>
-    </div>
-  );
-}
